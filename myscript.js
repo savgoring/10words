a2f7c4aa330adce16405011a6d153921f0eeae20
alexissrussell <alexissrussell@gmail.com>
2018-02-16 13:39:07 -0800

    added 10 word limit

--- myscript.js
+++ myscript.js
@@ -24,10 +24,20 @@ function publish(){
     postPage.style.display="none";
     homePage.style.display="block";
     
-}
+} 
     
+//10 word limit function
 
-
+$(document).ready(function() {
+    $("#storyInput").on('keydown', function(e) {
+        var words = $.trim(this.value).length ? this.value.match(/\S+/g).length : 0;
+        if (words <= 9) {
+            $('#word_left').text(9-words)
+        }else{
+            if (e.which !== 8) e.preventDefault();
+        }
+    });
+ }); 
 
 
 
